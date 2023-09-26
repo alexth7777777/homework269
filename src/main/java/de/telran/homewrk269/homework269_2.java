7130ca3d003ac6a9bc96812bf2cb805f09e581cd
Alex Thompson <alexth7777@gmail.com>
2023-09-26 21:38:33 +0200

    homework1

---
+++ src/main/java/de/telran/homewrk269/homework269_2.java
@@ -0,0 +1,22 @@
+package de.telran.homewrk269;
+
+public class homework269_2 {
+    public static void main(String[] args) {
+
+            char Character = 'g';
+            int Integer = 89;
+            byte Byte = 4;
+            short Short = 56;
+            float Float = 4.7333436F;
+            double Double = 4.355453532;
+            long Long = 12121;
+
+            System.out.println("Character:" + Character);
+            System.out.println("Integer:" + Integer);
+            System.out.println("Byte:"  + Byte);
+            System.out.println("Short:" + Short);
+            System.out.println("Float:" + Float);
+            System.out.println("Double:" + Double);
+            System.out.println("Long" + Long);
+    }
+}
---
+++ src/main/java/de/telran/homewrk269/task269.java
@@ -0,0 +1,19 @@
+package de.telran.homewrk269;
+
+public class task269 {
+    public static void main(String[] args) {
+        int number = 345;
+        int hundreds = (number/100);
+        int tens = (number / 10 % 10 );
+        int units = (number % 10);
+
+        System.out.println("hundreds:" + hundreds );
+        System.out.println("Tens:" + tens);
+        System.out.println("Units:" + units);
+
+    }
+}
+//2. * Дано трехзначное число. Вывести на экран все цифры этого числа
+//Пример: 345
+//Вывод в консоль: Число 345 -> 3, 4, 5
+
